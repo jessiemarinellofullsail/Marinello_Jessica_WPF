2144348e5884e3e66341d5d25ff39e73dd9d5973
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-15 18:42:26 -0400

    Work on Expressions_Industry math; add array of percentages for time you would like to spend on homework per night; test different days via console.log

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,5 +1,15 @@
 //alert("This is Expressions Industry");
 
 var totalHours = 28; //total amount of hours of school work to be completed in one week (five days)
-var totalHoursNight = totalHours / 5; //divide totalHours by number of days per week to get amount of hours needed per day
-//console.log(totalHoursNight);
+
+var percentageTime = [.3,.1,.25,.25,.1]; //percentage of time you want to spend on homework each night
+//30% on Mon., 10% on Tues., 25% on Wed., 25% on Thurs., 10% on Fri.
+
+//var monday = totalHours *= percentageTime[0]; //multi[ply totalHours and percentageTime to find out how many weighted hours you need to work that night in order to still complete all of your homework
+//console.log(monday);
+
+//var tuesday = totalHours *= percentageTime[1]; //multi[ply totalHours and percentageTime to find out how many weighted hours you need to work that night in order to still complete all of your homework
+//console.log(tuesday);
+
+var wednesday = totalHours *= percentageTime[2]; //multi[ply totalHours and percentageTime to find out how many weighted hours you need to work that night in order to still complete all of your homework
+console.log(wednesday);
