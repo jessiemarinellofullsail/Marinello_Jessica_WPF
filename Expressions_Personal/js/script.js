c9a62bc21933f75d184d004aa6efda758835d88a
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-15 19:52:17 -0400

    add comments to Expressions_Personal that include the formula for calculating BAC level

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,13 @@
 //Jessie Marinello
 //Expressions_Personal
 //May 2015
+
+//Estimate your BAC (Blood Alcohol Level)
+//BAC = (A*5.14/W*r) - 0.15*H)
+//Where A = total number of liquid ounces of alcohol consumed
+//Where 5.14 is a constant for average alcohol percent in beverage (can be changed if needed)
+//Where W = weight of person in pounds
+//Where r = alcohol distribution ratio; men = .73 and women = .66
+//Where H = number of hours drinks were consumed over
+//Where 0.15 is a constant for alcohol elimination rate
+
