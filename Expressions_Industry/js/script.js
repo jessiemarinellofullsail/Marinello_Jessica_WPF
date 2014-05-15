b97a265a2e2e081874026534c668a2112337dee1
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-15 18:20:20 -0400

    Add notes to Expressions Industry regarding first variables

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,5 +1,5 @@
 //alert("This is Expressions Industry");
 
-var totalHours = 28; 
-var totalHoursNight = totalHours / 5;
+var totalHours = 28; //total amount of hours of school work to be completed in one week (five days)
+var totalHoursNight = totalHours / 5; //divide totalHours by number of days per week to get amount of hours needed per day
 //console.log(totalHoursNight);
