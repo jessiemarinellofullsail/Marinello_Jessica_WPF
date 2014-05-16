3f1a3faebaea1c6474d8dbb7669c0f24a1cb7a00
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-15 20:27:48 -0400

    complete adding needed variables; test each math formula via console.log; add alert to print to user

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -14,4 +14,16 @@
 var drinkAmount = prompt("To calculate your BAC level, please enter the following information: \nThe total number of drinks consumed"); //user enters total number of drinks consumed
 var numOunces = prompt("Enter in the number of ounces in one drink, i.e. 12 ounces in one beer");
 var totalOunces = drinkAmount * numOunces;
-console.log(totalOunces);
+//console.log(totalOunces); test math
+var alcoholPercentage = prompt("Enter in the alcohol percentage in decimal format. This is usually located on the bottle");
+var a = drinkAmount * totalOunces * alcoholPercentage; //calculates info collected from prompts
+//console.log(a); test math
+const averageAlcohol = 5.14;
+var weight = prompt("Please enter your weight in pounds");
+var ratio = prompt("If you are a man enter .73. If you are a woman, enter .66");
+const elimination = 0.15;
+var hours = prompt("How many hours did you drink for?");
+var bacPercentage = (a * averageAlcohol / weight * ratio) - elimination * hours;
+//console.log(bacPercentage);
+
+alert("Your blood alcohol content is " + bacPercentage + " percent."); //prints to user
