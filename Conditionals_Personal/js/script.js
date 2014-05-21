ce8d832d3547300bbacf20251caf3869fe1574fb
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-21 16:57:15 -0400

    Create comments for Conditionals_Personal BMI Calculator, include formula

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,11 @@
 //Jessie Marinello
 //Conditionals_Personal
 //May 2015
+
+//BMI Calculator
+//Formula is as follows:
+//W/(H * H) *703
+//Where W=Weight in pounds
+//Where H=Height in inches
+//Where 703 = standard conversion factor for calculating BMI
+
