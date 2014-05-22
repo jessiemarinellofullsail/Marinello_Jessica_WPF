fff4de13d7dad30390965a40729ce085f3befc3c
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-22 13:57:42 -0400

    Enter in first set of variables; spend a million hours trying to figure out why height = heightConvert + inches was being treated as a string, solve issue by multiplying each variable by 1

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,3 +11,24 @@
 //Where H=Height in inches
 //Where 703 = standard conversion factor for calculating BMI
 
+/*var weight = prompt("Enter in your weight");
+var heightFeet = prompt("Enter in your height in feet");
+var heightInches = prompt("Enter in your height in inches");
+heightFeetConvert = heightFeet * 12;
+var height = heightFeetConvert + heightInches;
+if ( (weight <= 35) || (weight >= 500) || (height <= 48) || (height >= 120) ) {
+    alert("Invalid data.  Please check and re-enter!");
+}
+console.log(height);*/
+
+var weight = prompt("Enter in your weight");
+var heightFeet = prompt("Enter in your height in feet");
+var convertFeet = heightFeet * 12;
+var heightInches = prompt("Enter in your height in inches");
+var height = (convertFeet * 1) + (heightInches * 1 ); //multiply each number by 1 since original calculation was being treated as a string (original formula noted above)
+//console.log(height);
+if ( (weight <= 35) || (weight >= 500) || (height <= 48) || (height >= 120) ) {
+    //if weight is too small or too great, alert user
+    alert("Please check your weight and height and re-enter");
+}
+bmi = (Math.round((weight * 703) / (height * height)));
