e30f3e2c777722c15c707ea2ded53b78dfad755b
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-22 17:43:37 -0400

    Begin entering variables for Conditionals_Industry; include validation for user prompts

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,3 +5,13 @@
 
 //Do I have enough vacation days to leave work and go exploring???
 
+var totalTimeOff = prompt("Do you have enough vacation days to take a break? \nEnter in the total number of days off you are allotted");
+if (totalTimeOff === ""){
+    //if user did not input anything, prompt again
+    totalTimeOff = prompt("Don't forget to add your name!");
+}
+var totalDaysTaken = prompt("Enter in the number of vacation days you have already taken");
+if (totalDaysTaken === ""){
+    //if user did not input anything, prompt again
+    totalDaysTaken = prompt("Don't forget to add your name!");
+}
