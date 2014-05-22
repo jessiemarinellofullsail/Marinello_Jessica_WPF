68beef57688705a5227d9c91b59de1604e0fd2cd
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-22 15:25:00 -0400

    begin to add necessary variables; test character string.length property; test addition of same

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,3 +6,13 @@
 //LOOOOVEEE METER
 //Just because
 
+var yourName = prompt("What's your name?");
+var loversName = prompt("What's your lover's name?");
+var length1 = yourName.length;
+var length2 = loversName.length;
+//console.log(length1);
+//test string length property (found at http://www.w3schools.com/jsref/jsref_length_string.asp)
+
+totalLength = length1 + length2;
+//console.log(totalLength);
+//test addition of string length 1 and 2
