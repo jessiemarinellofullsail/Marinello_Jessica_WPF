0000265de75361070fc1280f8f38459209129c21
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-22 14:32:35 -0400

    Create if statements; realize that all BMI indexes cannot be if’s and change if else ifs since more than 3 blocks of code are running

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -10,6 +10,7 @@
 //Where W=Weight in pounds
 //Where H=Height in inches
 //Where 703 = standard conversion factor for calculating BMI
+//Information on BMI found at http://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html?s_cid=tw_ob064
 
 /*var weight = prompt("Enter in your weight");
 var heightFeet = prompt("Enter in your height in feet");
@@ -31,5 +32,19 @@ if ( (weight <= 35) || (weight >= 500) || (height <= 48) || (height >= 120) ) {
     //if weight is too small or too great, alert user
     alert("Please check your weight and height and re-enter");
 }
+
 bmi = (Math.round((weight * 703) / (height * height)));
 //console.log(bmi);
+if (bmi <19) {
+    alert("Underweight");
+} else if (bmi >=19 && bmi <=25){
+    alert("Your BMI is " + bmi + ", which is Desirable");
+} else if (bmi >=26 && bmi <=29) {
+    alert("Your BMI is " + bmi + ", which is Prone to health risks");
+} else if (bmi >=30 && bmi <=40) {
+    alert("Your BMI is " + bmi + ", which is Obese");
+} else if (bmi >40){
+    alert("Your BMI is " + bmi + ", which is Extremely obese");
+}
+
+
