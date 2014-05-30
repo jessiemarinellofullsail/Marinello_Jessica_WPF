ab0f8bdacf3ab983ca0f930f67920fbd049bf13c
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-30 13:32:10 -0400

    Add for loop to return amount of winning numbers requested by user; console.log to test; create alert to return user’s winning numbers!

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,8 +1,8 @@
-alert("i'm the weiner!");
+//alert("show me the money!");
 
 //Create a random lotto number generator to help you WIN BIG!
 
-var numbersNeeded = prompt("Enter in the amount of numbers you need"); //this number will be used for FOR LOOP to achieve requested amount of numbers
+var numbersNeeded = prompt("Enter in the amount of numbers you need"); //this number will be used for FOR LOOP to acheive requested amount of numbers
 //validate user inputs
 while (isNaN(numbersNeeded) || numbersNeeded === "" || numbersNeeded > 7) {
     //If conditions above are not met, prompt user to input a number value
@@ -19,18 +19,25 @@ var maximumNumbers = prompt("Enter in your maximum number range.");
 while (isNaN(maximumNumbers) || maximumNumbers === "") {
     //If conditions above are not met, prompt user to input a number value
     maximumNumbers = prompt("Don't forget to enter in your maximum number range.");
+}
 
 //console.log(numbersNeeded); validate user input with while loop and prompts
 //console.log(maximumNumbers); validate input with while loop and prompts
 
 //create function to choose numbers at random
 
-    function lottoNumbers (minimumNumber1, maximumNumbers1 ) {
-        var randomNumbers = Math.round( Math.random() * (maximumNumbers1 - minimumNumber1) + Number(minimumNumber1) );
-        //console.log(randomNumbers);
+function lottoNumbers (minimumNumber1, maximumNumbers1 ) {
+    var randomNumbers = Math.round( Math.random() * (maximumNumbers1 - minimumNumber1) + Number(minimumNumber1) );
+    //console.log(randomNumbers);
 
-        return randomNumbers;
-    }
+    return randomNumbers;
+}
 
 var yourNumbers = lottoNumbers(minimumNumber, maximumNumbers); //create variable to hold function in order to alert user
 //console.log(yourNumbers);
+
+for (var i = 0; i<numbersNeeded; i++){ //create for loop to return the amount of winning numbers user requested
+
+    alert("Your winning numbers are: " + "" + lottoNumbers (minimumNumber, maximumNumbers) ); //how to break up all of my random numbers in an alert or prompt?! tried researching without much luck.
+    //add function to loop in order for values to be returned
+}
