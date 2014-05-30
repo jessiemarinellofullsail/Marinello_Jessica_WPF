3979754750bdceca421807a24134912fe782f9a5
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-05-30 17:14:48 -0400

    add variables with user inputs; create validation while loops

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,21 @@
 
 //How many millions of years will it take you to pay off those student loans?
 
+var principalOwed = prompt("Please enter your total loan amount"); //prompt for value
+//validate user inputs
+while (isNaN(principalOwed) || principalOwed === "") {
+    //If conditions above are not met, prompt user to input a number value
+    principalOwed = prompt("Please enter a number value for your loan amount");
+}
+
+var monthlyPayment = prompt("Please enter your monthly payment"); //prompt for value
+while (isNaN(monthlyPayment) || monthlyPayment === "") {
+    //If conditions above are not met, prompt user to input a number value
+    monthlyPayment = prompt("Please enter a number value for your monthly payment");
+}
+
+var interestRate = prompt("Please enter your interest rate"); //prompt for value
+while (isNaN(interestRate) || interestRate === "") {
+    //If conditions above are not met, prompt user to input a number value
+    interestRate = prompt("Please enter a number value for your interest rate");
+}
